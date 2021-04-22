8573138e94f856ef6fbde8fe9e82080e7c70286a
Rodrigo R. Almeidam <rodrigo.roberto.almeida@gmail.com>
2021-04-22 02:41:27 -0300

    add hash to public users model

--- app/Models/PublicUser.js
+++ app/Models/PublicUser.js
@@ -1,5 +1,8 @@
 'use strict'
 
+/** @type {import('@adonisjs/framework/src/Hash')} */
+const Hash = use('Hash')
+
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
